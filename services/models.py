a9f6ad933b0f2ab2bc4b59cd4683973df8f815d9
ebarsula <ebarsulai@gmail.com>
2025-05-27 21:18:41 +0300

    Add ServiceHistory model to track customer service requests

--- services/models.py
+++ services/models.py
@@ -32,3 +32,12 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ServiceHistory(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    request_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.user.username} requested {self.service.name} on {self.request_date.strftime('%Y-%m-%d')}"
