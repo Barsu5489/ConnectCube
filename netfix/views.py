44da224520f50159e127270a3068948883fab2b3
ebarsula <ebarsulai@gmail.com>
2025-05-27 21:16:11 +0300

    Refactor customer_profile view: remove save, filter by Customer, and render service history

--- netfix/views.py
+++ netfix/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from users.models import User, Company
-from services.models import Service
+from users.models import User, Company, Customer
+from services.models import Service, ServiceHistory
 
 
 def home(request):
     return render(request, "users/home.html", {"user": request.user})
 
 
-def customer_profile(request):
-    pass
+
+def customer_profile(request, name):
+    # Get the Customer object by username of related User
+    customer = get_object_or_404(Customer, user__username=name)
+
+    # Filter ServiceHistory by the customer, NOT by user
+    service_history = ServiceHistory.objects.filter(customer=customer).order_by('-request_date')
+
+    return render(request, 'users/customer_profile.html', {
+        'customer': customer,
+        'service_history':service_history,
+    })
+
+
 
 
 def company_profile(request, name):
